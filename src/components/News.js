7aaa62b0cda8a84ecbe58aa9331d5d356477eb60
Rakshit Pandey <rakshit.lucky@gmail.com>
2023-03-02 14:42:13 +0530

    api

--- src/components/News.js
+++ src/components/News.js
@@ -3,21 +3,31 @@ import Navbar from './Navbar'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
+    constructor() {
+        super();
+        this.state = {
+            articles: [],
+            loading: false
+        }
+    }
+
+    async componentDidMount(){
+        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=78efd9bf075a475facb403f665cb50f1";
+        let data = await fetch(url);
+        let parssedData = await data.json();
+        this.setState({articles: parssedData.articles})
+    }
+
     render() {
         return (
             <div className="container my-3">
-                <h2>This is a News component</h2>
+                <h2>Top Headlines</h2>
                 <div className="row">
-                    <div className="col md-4">
-                        <NewsItem title="Title1" description="Description of Title1" imageUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1280px-Image_created_with_a_mobile_phone.png" />
+                    {this.state.articles.map((element)=>{
+                    return <div className="col md-4" key={element.url}>
+                        <NewsItem title={element.title?element.title.slice(0, 45):""} description={element.description?element.description.slice(0, 45):""} imageUrl={element.urlToImage} newsUrl={element.url} />
                     </div>
-                    <div className="col md-4">
-                        <NewsItem title="Title2" description="Description of Title2" imageUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1280px-Image_created_with_a_mobile_phone.png" />
-                    </div>
-                    <div className="col md-4">
-                        <NewsItem title="Title3" description="Description of Title3" imageUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1280px-Image_created_with_a_mobile_phone.png" />
-                    </div>
-
+                    })}
                 </div>
             </div>
         )
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,15 +1,8 @@
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
-    constructor() {
-        super();
-        this.state = {
-            articles: this.articles,
-            loading: false
-        }
-    }
     render() {
-        let { title, description, imageUrl } = this.props;
+        let { title, description, imageUrl, newsUrl } = this.props;
         return (
             <div>
                 <div className="card" style={{ width: "18rem" }}>
@@ -17,7 +10,7 @@ export class NewsItem extends Component {
                     <div className="card-body">
                         <h5 className="card-title">{title}</h5>
                         <p className="card-text">{description}</p>
-                        <a href="#" className="btn btn-primary">More details</a>
+                        <a href={newsUrl} target="_blank" className="btn btn-primary">More details</a>
                     </div>
                 </div>
             </div>
