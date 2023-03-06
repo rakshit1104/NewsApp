53f67d94f1fd277d5b95732f3a0ebf12756fff8c
Rakshit Pandey <rakshit.lucky@gmail.com>
2023-03-06 21:46:18 +0530

    change

--- src/components/News.js
+++ src/components/News.js
@@ -11,11 +11,11 @@ export class News extends Component {
         }
     }
 
-    async componentDidMount(){
+    async componentDidMount() {
         let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=78efd9bf075a475facb403f665cb50f1";
         let data = await fetch(url);
         let parssedData = await data.json();
-        this.setState({articles: parssedData.articles})
+        this.setState({ articles: parssedData.articles })
     }
 
     render() {
@@ -23,10 +23,10 @@ export class News extends Component {
             <div className="container my-3">
                 <h2>Top Headlines</h2>
                 <div className="row">
-                    {this.state.articles.map((element)=>{
-                    return <div className="col md-4" key={element.url}>
-                        <NewsItem title={element.title?element.title.slice(0, 45):""} description={element.description?element.description.slice(0, 45):""} imageUrl={element.urlToImage} newsUrl={element.url} />
-                    </div>
+                    {this.state.articles.map((element) => {
+                        return <div className="col md-4" key={element.url}>
+                            <NewsItem title={element.title ? element.title.slice(0, 45) : ""} description={element.description ? element.description.slice(0, 45) : ""} imageUrl={element.urlToImage} newsUrl={element.url} />
+                        </div>
                     })}
                 </div>
             </div>
